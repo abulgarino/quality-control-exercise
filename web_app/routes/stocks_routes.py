e38b29987202550752c553ba6d35f21e47740a39
abulgarino <101993477+abulgarino@users.noreply.github.com>
2024-12-04 13:05:26 -0500

    Revert "fix"
    This reverts commit 78867b0393bed4195a1317e0d8f4a624043dc850.

--- web_app/routes/stocks_routes.py
+++ web_app/routes/stocks_routes.py
@@ -12,7 +12,6 @@ def stocks_form():
     print("STOCKS FORM...")
     return render_template("stocks_form.html")
 
-#@stocks_routes.route("/stocks/dashboard", methods=["POST"])
 @stocks_routes.route("/stocks/dashboard", methods=["GET", "POST"])
 def stocks_dashboard():
     print("STOCKS DASHBOARD...")
@@ -23,20 +22,18 @@ def stocks_dashboard():
         print("FORM DATA:", request_data)
     else:
         # for data sent via GET request, url params are in request.args
-        request_data = dict(request.args) # /stocks/dashboard?symbol=GOOGL
+        request_data = dict(request.args)
         print("URL PARAMS:", request_data)
 
-    # grabbing some data from the dictionary sent by the form (or url params)
-    #risk_level = request_data.get("risk_level")
     symbol = request_data.get("symbol") or "NFLX"
 
     try:
         df = fetch_stocks_csv(symbol=symbol)
         latest_close_usd = format_usd(df.iloc[0]["adjusted_close"])
         latest_date = df.iloc[0]["timestamp"]
-        data = df.to_dict("records") # list of dictionaries!!
+        data = df.to_dict("records")
 
-        flash("Fetched Real-time Market Data!", "success")
+        #flash("Fetched Real-time Market Data!", "success")
         return render_template("stocks_dashboard.html",
             symbol=symbol,
             latest_close_usd=latest_close_usd,
@@ -46,7 +43,7 @@ def stocks_dashboard():
     except Exception as err:
         print('OOPS', err)
 
-        flash("Market Data Error. Please check your symbol and try again!", "danger")
+        #flash("Market Data Error. Please check your symbol and try again!", "danger")
         return redirect("/stocks/form")
 
 #
