745ed7bb53357fda698678cfba1f844f2df034fa
abulgarino <101993477+abulgarino@users.noreply.github.com>
2024-12-04 19:25:57 -0500

    fixed the nonalcoholic drinks API route and template file

--- web_app/routes/drink_routes.py
+++ web_app/routes/drink_routes.py
@@ -6,15 +6,17 @@ drink_routes = Blueprint("drink_routes", __name__)
 @drink_routes.route("/drinks/nonalcoholic")
 def nonalcoholic_drinks():
     try:
-        # Fetch nonalcoholic drinks from the Cocktail DB API
-        response = requests.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
-        response.raise_for_status()  # Raise exception for HTTP errors
-        data = response.json()
+        response = requests.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic") 
 
-        # Get the first 20 drinks
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+        #print(data)
+
         drinks = data.get("drinks", [])[:20]
+        #print(f"Drinks Extracted: {drinks}")
 
         return render_template("nonalcoholic_drinks.html", drinks=drinks)
     except Exception as err:
-        print(f"Error fetching drinks: {err}")
+        print(f"Error fetching nonalcoholic drinks: {err}")
         return "Error fetching nonalcoholic drinks", 500
+
